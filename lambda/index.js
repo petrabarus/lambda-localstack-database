5ee2980e307ff9bbb08dfdcbd2382a4ce137ac69
Petra Barus <petra.barus@kuncie.com>
2022-08-22 17:24:30 +0700

    solved

--- lambda/index.js
+++ lambda/index.js
@@ -1,21 +1,18 @@
-var mysql = require('mysql');
+const pg = require('pg');
+const R = require('ramda');
 
-var con = mysql.createConnection({
-    host: "mariadb",
-    user: "root",
-    password: "password",
-    database: "database"
-});
+const db_dsn = 'postgres://postgres:postgres@postgres:5432/postgres';
 
 exports.handler =  async function(event, context) {    
-
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-    });
+    const client = new pg.Client(db_dsn);
+    client.connect();
+    
+    const { rows  } = await client.query('SELECT 1 + 4');
+    const result = R.head(R.values(R.head(rows)));
+    client.end();
 
     return {
         statusCode: 200,
-        body: `Received: ${event.message}`
+        body: `Received: ${event.message}. Result: ${result}`
     };
 }
